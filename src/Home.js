81d356783c8ae48409e8cb63ae699783e36b000c
Sarthak Agrawal <agraw189@umn.edu>
2022-01-23 13:22:47 -0600

    fixed redirects

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,29 @@
 import Extension from "./Extension";
+import FadeIn from "react-fade-in";
 
 function Home() {
   return (
     <div class="Home">
       <Extension />
-      <div class="links">
-        <a
-          href="https://github.com/sarthyparty/fixword_ext"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Chrome Extension
-        </a>
-        <a
-          href="https://github.com/sarthyparty/fixword"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NPM Package
-        </a>
-      </div>
+      <FadeIn>
+        <div class="links">
+          <a
+            href="https://github.com/sarthyparty/fixword_ext"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Chrome Extension
+          </a>
+
+          <a
+            href="https://github.com/sarthyparty/fixword"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            NPM Package
+          </a>
+        </div>
+      </FadeIn>
     </div>
   );
 }
